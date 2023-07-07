09f9232b163195cde78857691b9119c4308d7c20
Michael Litchard <litchard.michael@gmail.com>
2023-07-07 07:14:08 +0000

    GID

---
+++ src/HauntedHouse/Game/GID.hs
@@ -0,0 +1,9 @@
+{-# LANGUAGE InstanceSigs #-}
+module HauntedHouse.Game.GID where
+
+newtype GID a 
+    = GID {unGID :: Int} deriving stock (Show,Ord,Eq)
+
+instance ToText (GID a) where
+    toText :: GID a -> Text
+    toText = show . unGID  
