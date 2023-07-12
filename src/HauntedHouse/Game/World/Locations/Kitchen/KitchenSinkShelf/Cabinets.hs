c7820bc18cc83f7d58b6e9a53724dde103cddc44
Michael Litchard <litchard.michael@gmail.com>
2023-07-12 12:46:24 +0000

    sketched out kitchen

---
+++ src/HauntedHouse/Game/World/Locations/Kitchen/KitchenSinkShelf/Cabinets.hs
@@ -0,0 +1,24 @@
+module HauntedHouse.Game.World.Locations.Kitchen.KitchenSinkShelf.Cabinets 
+  where
+import Data.These (These (..))
+import HauntedHouse.Game.GID
+import HauntedHouse.Game.Object
+
+kitchenShelfCabinetAbove :: Object
+kitchenShelfCabinetAbove = Object
+  { _container = kitchenShelfCabinetAboveContainerState
+  , _containedBy = Nothing
+  , _moveability = NotMovable
+  , _odescription = "A cabinet above the kitchen shelf"
+  }
+
+kitchenShelfCabinetAboveContainerState :: ContainerState
+kitchenShelfCabinetAboveContainerState = ContainerState 
+  (AttachedToObject (GID 0), Just (This kitchenCabinetAboveShelfContainer))
+
+kitchenCabinetAboveShelfContainer :: Container
+kitchenCabinetAboveShelfContainer = Container 
+  { _isOpen = Just False
+  , _cinv = Nothing
+  , _lockState = Nothing
+  }
